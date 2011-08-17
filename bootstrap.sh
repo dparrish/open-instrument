d75240610438a0bba26109f745848a9b75bf3a8e
David Parrish <david@dparrish.com>
2011-08-17 14:53:55 +1000

    Add bootstrap script to fetch dependencies

---
+++ bootstrap.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ ! -d static/protojs ]; then
+  pushd static
+  git clone https://github.com/sirikata/protojs.git protojs
+  cd protojs
+  ./bootstrap.sh
+  popd
+fi
+
+if [ ! -d static/flot ]; then
+  pushd static
+  svn checkout http://flot.googlecode.com/svn/trunk/ flot
+  popd
+fi
